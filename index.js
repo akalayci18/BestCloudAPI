e1168fcb8431b233fd9fa5973f22d4ed84acdb1a
akalayci18 <72552136+akalayci18@users.noreply.github.com>
2022-02-06 03:52:39 +0300

    temperature endpoint
    temperature?city=city_name prints temperature of the city using another API (https://openweathermap.org).

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const app = require('express')();
+const request = require('request-promise');
 const PORT = 5500;
 
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
 })
 
@@ -10,4 +11,28 @@ app.get('/', (req, res) => {
         firstname: 'Atahan',
         lastname: 'Kalayci'
     });
+});
+
+app.get('/temperature', (req, res) => {
+
+    let city=req.query.city;
+
+    var getTemp = {
+        method: 'GET',
+        url: `https://api.openweathermap.org/data/2.5/find?q=${city}&units=metric&appid=6b1336510c7a5a4d3256187801963143`,
+        json: true
+    };
+    //waits till API call is completed o.w returns an error in JSON format.
+    request(getTemp).then(
+        (response) => {
+            res.send({
+                temperature: response.list[0].main.temp
+            });
+        }
+    ).catch(err => {
+        res.send({
+            message: 'Internal Server Error'
+        });
+    })
+        
 });
